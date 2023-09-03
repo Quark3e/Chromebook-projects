55758773ea6a00d39878f408bed4a7e27490a701
Quark3e <b.erkhme@gmail.com>
2023-09-04 00:49:17 +0200

    new file

---
+++ teststuff/python/opencv/camDistMeth_compare/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import cv2 #type: ignore
+import socket
+import matplotlib.pyplot as plt
+import time
+import sys
+import socket
+import math
+import numpy as np
+from datetime import datetime
+
+
+def toRadians(degrees):
+    return (degrees*np.pi)/180
+def toDegrees(radians):
+    return (radians*180)/np.pi
+
+
+
+
+
+def main():
+    pass
+
+
+if __name__=="__main__":
+    main()
