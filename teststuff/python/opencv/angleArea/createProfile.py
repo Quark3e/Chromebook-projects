2d66de6c8f084bfe1315c362feabb9a8f070c3db
Quark3e <b.erkhme@gmail.com>
2023-10-28 00:48:22 +0200

    new img

--- teststuff/python/opencv/angleArea/createProfile.py
+++ teststuff/python/opencv/angleArea/createProfile.py
@@ -1723,7 +1723,7 @@ def opt5():
             ordVal[op2_settings["zPick"]["value"]][0], \
             ordVal[op2_settings["zPick"]["value"]][1] \
                    ])
-        plotCbs["slice"][0].set_array(ordVal[op2_settings["zPick"]["value"]][1])
+        plotCbs["slice"][0].set_array(ordVal[op2_settings["zPick"]["value"]][2])
         #validSaveFig(fig, "slice_"+str(numPoints)+"_z"+f'{op2_settings["zPick"]["value"]:03d}',dirPath["media"]["slices"]["path"],imgDpi=300,saveCopies=False)
         return plotCbs["ghost"][0], plotCbs["slice"][0]
 
