4b2b691ecba9d09680ecc9728228186d7ebc69b5
Quark3e <b.erkhme@gmail.com>
2023-10-28 01:41:08 +0200

    new img

--- teststuff/python/opencv/angleArea/createProfile.py
+++ teststuff/python/opencv/angleArea/createProfile.py
@@ -1711,7 +1711,7 @@ def opt5():
 
         print(" -creating colorbars")
         for key,items in plotCbs.items():
-            if key!="ghost": fig.colorbar(items[0], ax=ax[items[1]], location="left")
+            if key!="ghost": fig.colorbar(items[0], ax=ax[items[1]], location="right")
         return plotCbs["ghost"][0], plotCbs["slice"][0]
 
     def anim_update(i):
@@ -1719,11 +1719,8 @@ def opt5():
         op2_settings["zPick"]["value"]=i
         ghost[2]=[op2_settings["zPick"]["value"]]*len(ghost[0])
         plotCbs["ghost"][0]._offsets3d=(ghost[0],ghost[1],ghost[2])
-        plotCbs["slice"][0].set_offsets([ \
-            ordVal[op2_settings["zPick"]["value"]][0], \
-            ordVal[op2_settings["zPick"]["value"]][1] \
-                   ])
-        #plotCbs["slice"][0].set_array(ordVal[op2_settings["zPick"]["value"]][2])
+        plotCbs["slice"][0].set_offsets([ ordVal[op2_settings["zPick"]["value"]][0] ], [ ordVal[op2_settings["zPick"]["value"]][1] ])
+        plotCbs["slice"][0].set_array(ordVal[op2_settings["zPick"]["value"]][2])
         #validSaveFig(fig, "slice_"+str(numPoints)+"_z"+f'{op2_settings["zPick"]["value"]:03d}',dirPath["media"]["slices"]["path"],imgDpi=300,saveCopies=False)
         return plotCbs["ghost"][0], plotCbs["slice"][0]
 
