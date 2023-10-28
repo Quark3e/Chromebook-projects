7dd957e35bc42d49d1643ac2565dd76c60f0ec25
Quark3e <b.erkhme@gmail.com>
2023-10-28 16:19:10 +0200

    new

--- teststuff/python/opencv/angleArea/createProfile.py
+++ teststuff/python/opencv/angleArea/createProfile.py
@@ -1712,7 +1712,7 @@ def opt5():
         print(" -creating colorbars")
         for key,items in plotCbs.items():
             if key!="ghost": fig.colorbar(items[0], ax=ax[items[1]], location="right")
-        return plotCbs["ghost"][0], plotCbs["slice"][0]
+        return plotCbs["slice"][0], plotCbs["ghost"][0]
 
     def anim_update(i):
         print(i)
@@ -1723,7 +1723,7 @@ def opt5():
         plotCbs["slice"][0].set_offsets([ ordVal[op2_settings["zPick"]["value"]][0], ordVal[op2_settings["zPick"]["value"]][1] ])
         plotCbs["slice"][0].set_array(ordVal[op2_settings["zPick"]["value"]][2])
         #validSaveFig(fig, "slice_"+str(numPoints)+"_z"+f'{op2_settings["zPick"]["value"]:03d}',dirPath["media"]["slices"]["path"],imgDpi=300,saveCopies=False)
-        return plotCbs["ghost"][0], plotCbs["slice"][0]
+        return plotCbs["slice"][0], plotCbs["ghost"][0]
 
 
     # exit()
