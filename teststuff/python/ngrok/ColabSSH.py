9915c6d6bd41a9751e0f273bd7a46acf9e2f5bcd
Quark3e <b.erkhme@gmail.com>
2023-10-30 21:57:47 +0100

    new

--- teststuff/python/ngrok/ColabSSH.py
+++ teststuff/python/ngrok/ColabSSH.py
@@ -6,6 +6,37 @@ import os, time
 
 from pyngrok import ngrok, conf
 
+import digitalio #type: ignore
+import board #type: ignore
+from PIL import Image, ImageDraw, ImageFont
+from adafruit_rgb_display import st7735 #type: ignore
+
+BAUDRATE = 24_000_000
+
+BORDER = 20
+FONTSIZE = 24
+
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = digitalio.DigitalInOut(board.D24)
+
+spi = board.SPI()
+disp = st7735.ST7735R(spi, rotation=270, cs=cs_pin, dc=dc_pin, rst=reset_pin, baudrate=BAUDRATE)
+
+if disp.rotation % 180 == 90:
+    height = disp.width
+    width = disp.height
+else:
+    width = disp.width
+    height = disp.height
+
+image = Image.new("RGB", (width, height))
+draw = ImageDraw.Draw(image)
+draw.rectangle((0, 0, width, height), fill=(0, 0, 0))
+disp.image(image)
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", FONTSIZE)
+
+
 #print("Enter your authtoken, which can be copied from https://dashboard.ngrok.com/auth")
 #conf.get_default().auth_token = getpass.getpass()
 
@@ -13,7 +44,20 @@ from pyngrok import ngrok, conf
 connection_string = ngrok.connect("2223", "tcp").public_url
 
 ssh_url, port = connection_string.strip("tcp://").split(":")
-print(f" * ngrok tunnel available, access with `ssh pi@{ssh_url} -p{port}`")
+print(f"\n * ngrok tunnel available, access with `ssh pi@{ssh_url} -p{port}`")
+
+
+text = f"ssh pi@{ssh_url} -p{port}\n{ssh_url}:{port}"
+(font_width, font_height) = font.getsize(text)
+
+draw.text(
+    (width // 2- font_width // 2, height // 2 - font_height // 2),
+    text,
+    font=font,
+    fill=(255, 255, 255),
+)
+
+disp.image(image)
 
 ngrok_process = ngrok.get_ngrok_process()
 
