29a282fbe9356122e0d767ddf19d81f4320d3313
Quark3e <b.erkhme@gmail.com>
2023-12-24 15:37:17 +0100

    temp save

--- teststuff/electronics/accelerometers/adxl345/checkOffset.py
+++ teststuff/electronics/accelerometers/adxl345/checkOffset.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-import numpy as np
 import time
 import sys
 import os
 import os.path
-absPath = str(os.path.realpath(__file__)[:len(str(os.path.realpath(__file__)))-len("nodemcu_accel.py")])
-sys.path.append(absPath[:len(absPath)-len("teststuff/python/random/")]+"projects/")
+
+absPath = str(os.path.realpath(__file__)[:len(str(os.path.realpath(__file__)))-len("checkOffset.py")])
+sys.path.append(absPath[:len(absPath)-len("teststuff/electronics/accelerometers/adxl345/")]+"projects/")
 
 import proj_Hexclaw.IK_module as IK #type: ignore
 
