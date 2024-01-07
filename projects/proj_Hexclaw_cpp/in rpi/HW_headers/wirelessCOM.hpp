949c71303f43871c1294cd8d5e997e10f15ae161
Quark3e <b.erkhme@gmail.com>
2024-01-07 15:55:24 +0100

    temp push

--- projects/proj_Hexclaw_cpp/in rpi/HW_headers/wirelessCOM.hpp
+++ projects/proj_Hexclaw_cpp/in rpi/HW_headers/wirelessCOM.hpp
@@ -1,9 +1,28 @@
+
 #pragma once
 #ifndef H_WIRELESS_COM
 #define H_WIRELESS_COM
 
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <memory.h>
+#include <ifaddrs.h>
+#include <net/if.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <csignal>
 
+#define MAXLINE 2048
+
+
+class nodemcu_connect {
+    
+}
 
 
 #endif
