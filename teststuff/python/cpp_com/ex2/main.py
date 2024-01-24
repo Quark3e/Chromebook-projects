247b0607db41ef00b19ba39bfbb62ff35178d925
Quark3e <b.erkhme@gmail.com>
2024-01-24 16:21:09 +0100

    successful python-cpp interaction

---
+++ teststuff/python/cpp_com/ex2/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from subprocess import Popen, PIPE
+import os.path
+import sys
+
+absPath = os.path.realpath(__file__)[:-len("main.py")]
+
+p = Popen([absPath+'cppEXE'], shell=True, stdout=PIPE, stdin=PIPE)
+for ii in range(10):
+    value = str(ii) + '\n'
+    value = value.encode("utf-8")
+    #value = bytes(value, 'UTF-8')  # Needed in Python 3.
+    p.stdin.write(value)
+    p.stdin.flush()
+    result = p.stdout.readline().strip()
+    print(result.decode("utf-8"))
