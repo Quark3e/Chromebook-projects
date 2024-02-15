d2e08fcd68380cb4cc6b6e3ef0a47b942a882c2f
Quark3e <b.erkhme@gmail.com>
2024-02-16 00:00:39 +0100

    cum committed from Ch.

--- science/math/analytic_geometry/camTest/cpp_cameras.cpp
+++ science/math/analytic_geometry/camTest/cpp_cameras.cpp
@@ -13,7 +13,7 @@
 /*std::ios::sync_with_stdio(false);*/
 
 int main(int argc, char** argv) {
-    const bool useCamera = true, useTwoCamClass = true;
+    bool useCamera = true, useTwoCamClass = true;
 
     /*
     char array sent to stdout:
