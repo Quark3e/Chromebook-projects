eace025802174e9dad416a7208d40e610330caac
Quark3e <b.erkhme@gmail.com>
2024-02-16 01:24:56 +0100

    cum committed from Ch.

--- science/math/analytic_geometry/camTest/cpp_cameras.cpp
+++ science/math/analytic_geometry/camTest/cpp_cameras.cpp
@@ -67,8 +67,8 @@ int main(int argc, char** argv) {
 
         // IR_camTracking camObj[2] {
 
-        camObj.push_back(IR_camTracking(2, prefSize[0], prefSize[1], useAutoBrightne, displayToWindow, takePerformance););
-        camObj.push_back(IR_camTracking(0, prefSize[0], prefSize[1], useAutoBrightne, displayToWindow, takePerformance););
+        camObj.push_back(IR_camTracking(2, prefSize[0], prefSize[1], useAutoBrightne, displayToWindow, takePerformance));
+        camObj.push_back(IR_camTracking(0, prefSize[0], prefSize[1], useAutoBrightne, displayToWindow, takePerformance));
         
         
         camObj[0].setup_window();
