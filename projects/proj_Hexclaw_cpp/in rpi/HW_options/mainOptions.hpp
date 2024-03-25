b4044f3fb3057af3fabd104244bec94a699132dd
Quark3e <b.erkhme@gmail.com>
2024-03-25 01:30:03 +0100

    cum committed from pi

--- projects/proj_Hexclaw_cpp/in rpi/HW_options/mainOptions.hpp
+++ projects/proj_Hexclaw_cpp/in rpi/HW_options/mainOptions.hpp
@@ -7,11 +7,19 @@
 
 #include "../../../../teststuff/cpp/useful/stringToFunc.hpp"
 
+
+
 StringToFunction hexclaw_cmdArgs{};
 
 
+void cmdArgs_info() {}
+
+void option0() {}
 
 void HW_setup_options() {
+    hexclaw_cmdArgs.add_func("-h", cmdArgs_info, "--help");
+    hexclaw_cmdArgs.add_func("-m", option0, "--main");
+
 
 
 }
