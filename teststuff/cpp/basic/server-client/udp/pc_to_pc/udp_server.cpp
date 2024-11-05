ec51850c23facffe53158873768ec8f283eb1a70
Quark3e <b.erkhme@gmail.com>
2024-11-05 23:37:41 +0100

    test

--- teststuff/cpp/basic/server-client/udp/pc_to_pc/udp_server.cpp
+++ teststuff/cpp/basic/server-client/udp/pc_to_pc/udp_server.cpp
@@ -11,30 +11,29 @@
 #define PORT    8080
 #define MAXLINE 1024
 
-// Driver code 
-int main() { 
-	int sockfd; 
-	char buffer[MAXLINE]; 
-	const char *hello = "Hello from server"; 
-	struct sockaddr_in servaddr, cliaddr; 
-	
-	// Creating socket file descriptor 
-	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
-		perror("socket creation failed"); 
-		exit(EXIT_FAILURE); 
-	} 
-	
-	memset(&servaddr, 0, sizeof(servaddr)); 
-	memset(&cliaddr, 0, sizeof(cliaddr)); 
-	
-	// Filling server information 
-	servaddr.sin_family = AF_INET; // IPv4 
-	servaddr.sin_addr.s_addr = INADDR_ANY; 
-	servaddr.sin_port = htons(PORT); 
+// Driver code
+int main() {
+	int sockfd;
+	char buffer[MAXLINE];
+	const char *hello = "Hello from server";
+	struct sockaddr_in servaddr, cliaddr;
 
-	// Bind the socket with the server address 
-	if(bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0 ) 
-	{
+	// Creating socket file descriptor 
+	if((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+		perror("socket creation failed");
+		exit(EXIT_FAILURE);
+	}
+
+	memset(&servaddr, 0, sizeof(servaddr));
+	memset(&cliaddr, 0, sizeof(cliaddr));
+
+	// Filling server information
+	servaddr.sin_family = AF_INET; // IPv4
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+	servaddr.sin_port = htons(PORT);
+
+	// Bind the socket with the server address
+	if(bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
 		perror("bind failed");
 		exit(EXIT_FAILURE);
 	}
@@ -42,13 +41,30 @@ int main() {
 	socklen_t len;
     int n;
 
-	len = sizeof(cliaddr); //len is value/result 
+	len = sizeof(cliaddr); //len is value/result
 
-	n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &cliaddr, &len);
-	buffer[n] = '\0';
-	printf("Client : %s\n", buffer);
-	sendto(sockfd, (const char *)hello, strlen(hello), MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len);
-	std::cout<<"Hello message sent."<<std::endl;
+    while(true) {
+	    n = recvfrom(
+            sockfd,
+            (char *)buffer,
+            MAXLINE,
+            MSG_WAITALL,
+            (struct sockaddr *) &cliaddr,
+            &len
+        );
+	    buffer[n] = '\0';
+	    printf("Client : %s\n", buffer);
+	    sendto(
+            sockfd,
+            (const char *)hello,
+            strlen(hello),
+            MSG_CONFIRM,
+            (const struct sockaddr *) &cliaddr,
+            len
+        );
+	    std::cout<<"Hello message sent."<<std::endl;
+    }
+    close(sockfd);
 
 	return 0;
 }
