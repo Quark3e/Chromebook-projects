1bac9ce2c0031c5fec3742a3c1806ecde23afedc
Quark3e <b.erkhme@gmail.com>
2024-12-14 16:00:25 +0100

    added some explaining comments found in the source

--- teststuff/cpp/windows/list_files_in_dir/main.cpp
+++ teststuff/cpp/windows/list_files_in_dir/main.cpp
@@ -1,3 +1,11 @@
+/**
+ * @file main.cpp
+ * @author https://learn.microsoft.com/sv-se/windows/win32/fileio/listing-the-files-in-a-directory?redirectedfrom=MSDN
+ * @brief List all files and directories in a directory
+ * @version 0.1
+ * @date 2024-12-14
+ * 
+ */
 
 #include <windows.h>
 #include <tchar.h>
@@ -16,11 +24,15 @@ int _tmain(int argc, TCHAR *argv[]) {
     HANDLE          hFind = INVALID_HANDLE_VALUE;
     DWORD           dwError = 0;
 
+    // If the directory is not specified as a command-line argument,
+    // print usage.
     if(argc!=2) {
         _tprintf(TEXT("\nUsage: %s <directory name>\n"), argv[0]);
         return -1;
     }
 
+    // Check that the input path plus 3 is not longer than MAX_PATH.
+    // Three characters are for the "\*" plus NULL appended below.
     StringCchLength(argv[1], MAX_PATH, &length_of_arg);
     if(length_of_arg > (MAX_PATH - 3)) {
         _tprintf(TEXT("\nDirectory path is too long.\n"));
@@ -30,9 +42,12 @@ int _tmain(int argc, TCHAR *argv[]) {
 
     _tprintf(TEXT("\nTarget directory is %s\n\n"), argv[1]);
 
+    // Prepare string for use with FindFile functions.  First, copy the
+    // string to a buffer, then append '\*' to the directory name.
     StringCchCopy(szDir, MAX_PATH, argv[1]);
     StringCchCat(szDir, MAX_PATH, TEXT("\\*"));
 
+    // Find the first file in the directory.
     hFind = FindFirstFile(szDir, &ffd);
 
     if(INVALID_HANDLE_VALUE == hFind) {
@@ -40,6 +55,7 @@ int _tmain(int argc, TCHAR *argv[]) {
         return dwError;
     }
 
+    // List all the files in the directory with some info about them.
     do {
         if(ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
             _tprintf(TEXT("  %s  <DIR>\n"), ffd.cFileName);
@@ -62,6 +78,7 @@ int _tmain(int argc, TCHAR *argv[]) {
 }
 
 void DisplayErrorBox(LPTSTR lpszFunction) {
+    // Retrieve the system error for the last-error code
     LPVOID lpMsgBuf;
     LPVOID lpDisplayBuf;
     DWORD dw = GetLastError();
@@ -78,6 +95,7 @@ void DisplayErrorBox(LPTSTR lpszFunction) {
         NULL
     );
 
+    // Display the error message and clean up
     lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT, (lstrlen((LPCTSTR)lpMsgBuf)+lstrlen((LPCTSTR)lpszFunction)+40)*sizeof(TCHAR));
     StringCchPrintf((LPTSTR)lpDisplayBuf, LocalSize(lpDisplayBuf)/sizeof(TCHAR), TEXT("%s failed with error %d: %s"), lpszFunction, dw, lpMsgBuf);
     MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK);
