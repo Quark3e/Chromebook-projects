4a729ded45774cdf95632ed0672762660f7531e1
Quark3e <b.erkhme@gmail.com>
2025-01-05 19:29:47 +0100

    got networking/udp/udpCOM_toESP8266/udpCOM.cpp working without resolve_helper and should be ready to port to winsock2.. apparently client don't need to be bound... cool

--- teststuff/cpp/basic/networking/udp/udpCOM_toESP8266/udpCOM.cpp
+++ teststuff/cpp/basic/networking/udp/udpCOM_toESP8266/udpCOM.cpp
@@ -20,30 +20,32 @@
 
 #define MAXLINE 2048
 
-int resolvehelper(const char* hostname, int family, const char* service, sockaddr_storage* pAddr)
-{
-    int result;
-    addrinfo* result_list = NULL;
-    addrinfo hints = {};
-    hints.ai_family = family;
-    hints.ai_socktype = SOCK_DGRAM; // without this flag, getaddrinfo will return 3x the number of addresses (one for each socket type).
-    result = getaddrinfo(hostname, service, &hints, &result_list);
-    if (result == 0)
-    {
-        //ASSERT(result_list->ai_addrlen <= sizeof(sockaddr_in));
-        memcpy(pAddr, result_list->ai_addr, result_list->ai_addrlen);
-        freeaddrinfo(result_list);
-    }
+// int resolvehelper(const char* hostname, int family, const char* service, sockaddr_storage* pAddr)
+// {
+//     int result;
+//     addrinfo* result_list = NULL;
+//     addrinfo hints = {};
+//     hints.ai_family = family;
+//     hints.ai_socktype = SOCK_DGRAM; // without this flag, getaddrinfo will return 3x the number of addresses (one for each socket type).
+//     result = getaddrinfo(hostname, service, &hints, &result_list);
+//     if (result == 0)
+//     {
+//         //ASSERT(result_list->ai_addrlen <= sizeof(sockaddr_in));
+//         memcpy(pAddr, result_list->ai_addr, result_list->ai_addrlen);
+//         freeaddrinfo(result_list);
+//     }
 
-    return result;
-}
+//     return result;
+// }
 
 
 
 int main() {
+    printf("-----PROGRAM START-----\n");
+
     int result = 0;
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
-    fcntl(sock, F_SETFL, O_NONBLOCK);
+    // fcntl(sock, F_SETFL, O_NONBLOCK);
     char buffer[MAXLINE];
     const char* PORT = "1089";
     const char* IPADDR = "192.168.1.117"; //"192.168.1.231" - other board
@@ -53,20 +55,22 @@ int main() {
 
     sockaddr_in addrListen = {}; // zero-int, sin_port is 0, which picks a random port for bind.
     addrListen.sin_family = AF_INET;
-    result = bind(sock, (sockaddr*)&addrListen, sizeof(addrListen));
-    if (result == -1) {
-       int lasterror = errno;
-       std::cout << "error: " << lasterror;
-       exit(1);
-    }
+    addrListen.sin_addr.s_addr = inet_addr(IPADDR);
+    addrListen.sin_port = htons(1089);
+    // result = bind(sock, (sockaddr*)&addrListen, sizeof(addrListen));
+    // if (result == -1) {
+    // //    int lasterror = errno;
+    //    perror("bind error: ");
+    //    exit(1);
+    // }
 
-    sockaddr_storage addrDest = {};
-    result = resolvehelper(IPADDR, AF_INET, PORT, &addrDest);
-    if (result != 0) {
-       int lasterror = errno;
-       std::cout << "error: " << lasterror;
-       exit(1);
-    }
+    // sockaddr_storage addrDest = {};
+    // result = resolvehelper(IPADDR, AF_INET, PORT, &addrDest);
+    // if (result != 0) {
+    //    int lasterror = errno;
+    //    std::cout << "error: " << lasterror;
+    //    exit(1);
+    // }
     
 
     const char* msg = "1";
@@ -74,13 +78,13 @@ int main() {
         usleep(10'000);
         clock_t t1 = clock();
         size_t msg_length = strlen(msg);
-        result = sendto(sock, msg, msg_length, 0, (sockaddr*)&addrDest, sizeof(addrDest));
+        result = sendto(sock, msg, msg_length, 0, (sockaddr*)&addrListen, sizeof(addrListen));
 
         // std::cout << result << " bytes sent" << std::endl;
         printf("%d bytes sent.\t", result);
         socklen_t len;
 
-        int n = recvfrom(sock, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &addrDest, &len);
+        int n = recvfrom(sock, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &addrListen, &len);
         buffer[n] = '\0';
         std::cout << buffer[n-1] << "\t";
         // std::cout<<"Server :"<<buffer<<std::endl;
