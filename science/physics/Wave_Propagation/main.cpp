2bee9d3de73851c6e1fb60dc3a9846f237ada121
Quark3e <b.erkhme@gmail.com>
2025-03-05 16:42:48 +0100

    file correction

--- science/physics/Wave_Propagation/main.cpp
+++ science/physics/Wave_Propagation/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 #include <imguiwin.hpp>
 
@@ -39,28 +38,3 @@ int main(int argc, char** argv) {
 }
 
 
-=======
-
-#include <imguiwin.hpp>
-#include <pos2d.hpp>
-
-#include <vector>
-#include <cmath>
-
-#include <keyBind_handler.hpp>
-#include <nodeChart/extra_imgui.hpp>
-
-#include <useful.hpp>
-
-#define unit(val) (val)/abs(val)
-
-
-
-
-int main(int argc, char** argv) {
-
-
-
-    return 0;
-}
->>>>>>> 6c02d61 (some minor changes + got imguiwin to build/compile successfully on chromeos unit for both projects nad hte lib itself.)
