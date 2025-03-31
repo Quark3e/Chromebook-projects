4635e9f5e4c3ed3ff3c9a3d066426a899d3383d1
Quark3e <b.erkhme@gmail.com>
2025-03-31 15:55:30 +0200

    minor work into servo motor calibration option.

--- projects/proj_Hexclaw_cpp/Hexclaw_Main/src/hw_option_4.cpp
+++ projects/proj_Hexclaw_cpp/Hexclaw_Main/src/hw_option_4.cpp
@@ -26,25 +26,30 @@ TUI::termMenu menu__calibrateMotor_main({
 
 TUI::termMenu menu__calibrateMotor_angles({
 
-	{"  0°", 1,  2, -1}, {"   ", 2, 2, -1},
-	{" 10°", 1,  2, -1}, {"   ", 2, 2, -1},
-	{" 20°", 1,  2, -1}, {"   ", 2, 2, -1},
-	{" 30°", 1,  2, -1}, {"   ", 2, 2, -1},
-	{" 40°", 1,  2, -1}, {"   ", 2, 2, -1},
-	{" 50°", 1,  2, -1}, {"   ", 2, 2, -1},
-	{" 60°", 1,  2, -1}, {"   ", 2, 2, -1},
-	{" 70°", 1,  2, -1}, {"   ", 2, 2, -1},
-	{" 80°", 1,  2, -1}, {"   ", 2, 2, -1},
-	{" 90°", 1,  2, -1}, {"   ", 2, 2, -1},
-	{"100°", 1,  2, -1}, {"   ", 2, 2, -1},
-	{"120°", 1,  2, -1}, {"   ", 2, 2, -1},
-	{"130°", 1,  2, -1}, {"   ", 2, 2, -1},
-	{"140°", 1,  2, -1}, {"   ", 2, 2, -1},
-	{"150°", 1,  2, -1}, {"   ", 2, 2, -1},
-	{"160°", 1,  2, -1}, {"   ", 2, 2, -1},
-	{"170°", 1,  2, -1}, {"   ", 2, 2, -1},
-	{"180°", 1,  2, -1}, {"   ", 2, 2, -1}
-});
+	{"  0°", 1,  2, -1}, {"   ", 2,  2, -1},
+	{" 10°", 1,  3, -1}, {"   ", 2,  3, -1},
+	{" 20°", 1,  4, -1}, {"   ", 2,  4, -1},
+	{" 30°", 1,  5, -1}, {"   ", 2,  5, -1},
+	{" 40°", 1,  6, -1}, {"   ", 2,  6, -1},
+	{" 50°", 1,  7, -1}, {"   ", 2,  7, -1},
+	{" 60°", 1,  8, -1}, {"   ", 2,  8, -1},
+	{" 70°", 1,  9, -1}, {"   ", 2,  9, -1},
+	{" 80°", 1, 10, -1}, {"   ", 2, 10, -1},
+	{" 90°", 1, 11, -1}, {"   ", 2, 11, -1},
+	{"100°", 1, 12, -1}, {"   ", 2, 12, -1},
+	{"120°", 1, 13, -1}, {"   ", 2, 13, -1},
+	{"130°", 1, 14, -1}, {"   ", 2, 14, -1},
+	{"140°", 1, 15, -1}, {"   ", 2, 15, -1},
+	{"150°", 1, 16, -1}, {"   ", 2, 16, -1},
+	{"160°", 1, 17, -1}, {"   ", 2, 17, -1},
+	{"170°", 1, 18, -1}, {"   ", 2, 18, -1},
+	{"180°", 1, 19, -1}, {"   ", 2, 19, -1},
+
+	{"Solve",1, 20, -1},
+	
+	{"back", 1, 22, -1},
+	{"exit", 1, 23, -1}
+}, false);
 
 void HW_option4() {
 	// simplified_init();
@@ -67,10 +72,53 @@ void HW_option4() {
 		pos2d<int> pressed_pos = menu__calibrateMotor_main.driver(0, 5, 1, false, TUI::termMenu_nullFunc_void__, false);
 
 		if(pressed_pos==pos2d<int>(0, 9)) break;
-		if(pressed_pos.inRegion({0, 2}, {0, 7})) {
+		if(pressed_pos.inRegion({0, 2}, {0, 7})) { //check if pressed_pos is in region of the selectable motor options
 			ANSI_mvprint(0, 2, "Calibrating motor: q["+formatNumber(pressed_pos[1]-2)+"]", true, "abs", "abs", true);
 			std::this_thread::sleep_for(std::chrono::seconds(1));
 			
+			// calibrate motor
+			bool runAngles = true;
+			while(runAngles) {
+				ANSI_mvprint(0, 0, "Select an angle to calibrate", true, "abs", "rel");
+				pos2d<int> pressed_pos = menu__calibrateMotor_angles.driver(0, 5, 1, false, TUI::termMenu_nullFunc_void__, false);
+				if(pressed_pos==pos2d<int>(0, 9)) break;
+				if(pressed_pos.inRegion({1, 2}, {1, 19})) { //check if pressed_pos is in region of the selectable motor options
+					orientObj.update(false);
+
+					menu__calibrateMotor_angles.rename_opt(2, pressed_pos[1], formatNumber(orient.y, 7, 1));
+
+					/// Manual input of angles
+					// std::string inpValue = menu__calibrateMotor_angles.termInput(0);
+					// if(inpValue=="exit") return;
+					// else if(inpValue=="back") break;
+					// else {
+					// 	menu__calibrateMotor_angles.rename_opt(2, pressed_pos[1], inpValue);
+					// }
+				}
+				else if(pressed_pos==pos2d<int>(1, 20)) {
+					std::vector<float> readAngles(19, 0);
+					for(size_t i=0; i<19; i++) {
+						try {
+							readAngles[i] = std::stof(menu__calibrateMotor_angles.getCellName(2, i+2));
+						}
+						catch(const std::invalid_argument& e) {
+							ANSI_mvprint(0, 0, "ERROR: invalid input", true, "abs", "rel");
+							std::this_thread::sleep_for(std::chrono::seconds(1));
+							break;
+						}
+					}
+
+
+				}
+				else if(pressed_pos==pos2d<int>(1, 22)) {
+					runAngles = false;
+				}
+				else if(pressed_pos==pos2d<int>(1, 23)) {
+					runAngles = false;
+					runMain = false;
+				}
+
+			}
 		}
 	}
 
