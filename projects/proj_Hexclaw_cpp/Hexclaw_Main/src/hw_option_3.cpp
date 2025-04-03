3ca1d00341a66b8fbf65e4f58a918c62948c2606
Quark3e <b.erkhme@gmail.com>
2025-04-03 23:31:37 +0200

    did a quick fix for option 3

--- projects/proj_Hexclaw_cpp/Hexclaw_Main/src/hw_option_3.cpp
+++ projects/proj_Hexclaw_cpp/Hexclaw_Main/src/hw_option_3.cpp
@@ -14,7 +14,7 @@
 
 
 void HW_option3() {
-	// simplified_init();
+
 	bool init_camObj = _init_status.get("camObj").isInit();
 	bool init_orientObj = _init_status.get("orientObj").isInit();
 	if(!init_camObj && !init_orientObj) {
@@ -41,17 +41,29 @@ void HW_option3() {
 	#if useThreads
 	std::unique_lock<std::mutex> u_lck0(mtx[0], std::defer_lock);
 	std::unique_lock<std::mutex> u_lck1(mtx[1], std::defer_lock);
-	std::unique_lock<std::mutex> u_lck_cout(mtx_cout, std::defer_lock);
-
-	std::thread t0(thread_task, &camObj[0], 0);
-	std::thread t1(thread_task, &camObj[1], 1);
-	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	if(init_camObj) {
+		std::unique_lock<std::mutex> u_lck_cout(mtx_cout, std::defer_lock);
+	
+		std::thread t0(thread_task, &camObj[0], 0);
+		std::thread t1(thread_task, &camObj[1], 1);
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	}
 	#endif //useThreads
 
+
+	/// Initialise the camera objects and the threads
+
+	// camObj/thread initialisation loop boolean.
 	bool loopInit = false;
 	while(true) {
-		if(!threadsInit[2]) {
+		/// Main option loop
+		
+		#if useThreads
+		if(init_camObj) {
 			while(!threadsInit[2]) {
+				/// While the main thread isn't initialised, keep checking the sub-threads
+				/// and update the camera variables
+				/// until both the subthreads are initialised and main thread can be declared as initialised.
 				u_lck0.lock();
 				u_lck1.lock();
 				updateCamVars(0);
@@ -62,58 +74,81 @@ void HW_option3() {
 				u_lck1.unlock();
 				u_lck0.unlock();
 			}
-		}
 
-		u_lck0.lock();
-		updateCamVars(0);
-		u_lck0.unlock();
-		u_lck1.lock();
-		updateCamVars(1);
-		u_lck1.unlock();
-		if(returCodes_main[0]==-1) {
-			if(threadDebug) lock_cout(mtx_cout,"\ncamObj[0] process error\n",true,true);
-			return;
+			u_lck0.lock();
+			updateCamVars(0);
+			u_lck0.unlock();
+			u_lck1.lock();
+			updateCamVars(1);
+			u_lck1.unlock();
+			if(returCodes_main[0]==-1) {
+				if(threadDebug) lock_cout(mtx_cout,"\ncamObj[0] process error\n",true,true);
+				return;
+			}
+			if(returCodes_main[1]==-1) {
+				if(threadDebug) lock_cout(mtx_cout,"\ncamObj[1] process error\n",true,true);
+				return;
+			}
 		}
-		if(returCodes_main[1]==-1) {
-			if(threadDebug) lock_cout(mtx_cout,"\ncamObj[1] process error\n",true,true);
-			return;
-		}
+		#endif //useThreads
 
-		if(numContours_main[0]>0) {
-			printTable.insertNum(PP[0],1,1,1);
-			printTable.insertNum(PP[1],2,1,1);
-			printTable.insertNum(PP[2],3,1,1);
-
+		printTable.insertNum(PP[0],1,1,1);
+		printTable.insertNum(PP[1],2,1,1);
+		printTable.insertNum(PP[2],3,1,1);
+		
+		try {
 			orientObj.update(false);
-			printTable.insertNum(orient[0],1,2,1);
-			printTable.insertNum(orient[1],2,2,1);
-			printTable.insertNum(orient[2],3,2,1);
+			printTable.insertNum(orientObj.Roll,1,2,1);
+			printTable.insertNum(orientObj.Pitch,2,2,1);
+			printTable.insertNum(0,3,2,1);
 
-			printTable.insertNum(camObjPos_main[0][0],1,0,1);
-			printTable.insertNum(camObjPos_main[0][1],2,0,1);
-			printTable.insertNum(camObjPos_main[1][0],3,0,1);
-			printTable.insertNum(camObjPos_main[1][1],4,0,1);
+			orient[0] = orientObj.Roll;
+			orient[1] = orientObj.Pitch;
 		}
-		else {
-			printTable.insertText("ERROR:no valid orient", 1, 1);
+		catch(const std::exception& e) {
+			printTable.insertText("ERROR:", 1, 2);
+			printTable.insertText(e.what(), 2, 2);
+			// std::cerr << e.what() << '\n';
+		}
+		
+		
+		#if useThreads
+		if(init_camObj) {
+			if(numContours_main[0]>0) {
+				printTable.insertNum(camObjPos_main[0][0],1,0,1);
+				printTable.insertNum(camObjPos_main[0][1],2,0,1);
+				printTable.insertNum(camObjPos_main[1][0],3,0,1);
+				printTable.insertNum(camObjPos_main[1][1],4,0,1);
+			}
+			else {
+				printTable.insertText("ERROR:no tracked object visible on cams", 1, 1);
+			}
+		}
+		#endif //useThreads
+
+		try {
+			if(HW_KINEMATICS::getAngles(new_q, PP, RADIANS(orient[0]), RADIANS(orient[1]), RADIANS(orient[2]), 1)) {
+				for(int i=0; i<6; i++) printTable.insertNum(new_q[i],1+i,3,1);
+				printTable.strExport("");
+			}
+			else if(HW_KINEMATICS::findValidOrient(PP, orient, orient, new_q)) {
+				printTable.insertNum(orient[0],1,2,1);
+				printTable.insertNum(orient[1],2,2,1);
+				printTable.insertNum(orient[2],3,2,1);
+				for(int i=0; i<6; i++) printTable.insertNum(new_q[i],1+i,3,1);
+				printTable.strExport(std::string(21,' ')+"\n");
+			}
+			else {
+				printTable.insertText("ERROR:no valid angles", 1, 3);
+			}
+		}
+		catch(const std::exception& e) {
+			printTable.insertText("ERROR:", 1, 3);
+			printTable.insertText(e.what(), 2, 3);
+			// std::cerr << e.what() << '\n';
 		}
 
-		if(HW_KINEMATICS::getAngles(new_q, PP, RADIANS(orient[0]), RADIANS(orient[1]), RADIANS(orient[2]), 1)) {
-			for(int i=0; i<6; i++) printTable.insertNum(new_q[i],1+i,3,1);
-			printTable.strExport(std::string(21,' ')+"\n");
-		}
-		else if(HW_KINEMATICS::findValidOrient(PP, orient, orient, new_q)) {
-			printTable.insertNum(orient[0],1,2,1);
-			printTable.insertNum(orient[1],2,2,1);
-			printTable.insertNum(orient[2],3,2,1);
-			for(int i=0; i<6; i++) printTable.insertNum(new_q[i],1+i,3,1);
-			printTable.strExport(std::string(21,' ')+"\n");
-		}
-		else {
-			printTable.insertText("ERROR:no valid angles", 1, 3);
-		}
-
-		if(_CONFIG_OPTIONS.get("displayToWindow")) {
+		if(init_camObj && _CONFIG_OPTIONS.get("displayToWindow")) {
 			cv::Mat winImg;
 			cv::hconcat(flippedImg_main[0], flippedImg_main[1], winImg);
 			cv::imshow("Main thread window", winImg);
@@ -153,10 +188,12 @@ void HW_option3() {
 
 		}
 		
+		ANSI_mvprint(0, 4, printTable.exportStr, true, "abs", "abs");
+
 		loopInit = true;
 	}
 
-	if(_CONFIG_OPTIONS.get("displayToWindow")) {
+	if(init_camObj && _CONFIG_OPTIONS.get("displayToWindow")) {
 		cv::destroyWindow("Main thread window");
 	}
 
