232a8c06b300603f0a0715b9fb5f5e8a16499ab2
Quark3e <b.erkhme@gmail.com>
2025-04-17 20:38:35 +0200

    Need to consider creating an IMU reading thread library so that the orientObj client request timeouts can be handled in a separate thread.

---
+++ projects/proj_Hexclaw_cpp/Hexclaw_Main/lib/ThreadedIMU/ThreadedIMU.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef HPP_LIB__THREADEDIMU
+#define HPP_LIB__THREADEDIMU
+
+
+class ThreadedIMU {
+    private:
+    
+    public:
+    ThreadedIMU(/* args */);
+    ~ThreadedIMU();
+};
+
+ThreadedIMU::ThreadedIMU(/* args */) {
+
+}
+
+ThreadedIMU::~ThreadedIMU() {
+
+}
+
+
+#endif
