cfb908587da54f873ccf61aebf23bc64784987d7
Quark3e <b.erkhme@gmail.com>
2025-04-17 16:33:05 +0200

    random unsuccessful attempt att trying to build a protocol buffer project

--- teststuff/cpp/protocol_buffer/basic_examples/addressbook/DST_DIR/message_write.cpp
+++ teststuff/cpp/protocol_buffer/basic_examples/addressbook/DST_DIR/message_write.cpp
@@ -1,94 +1,93 @@
 #include <iostream>
-// #include <fstream>
-// #include <string>
-// #include "addressbook.pb.h"
+#include <fstream>
+#include <string>
+#include "addressbook.pb.h"
 using namespace std;
 
-// // This function fills in a Person message based on user input.
-// void PromptForAddress(tutorial::Person* person) {
-//     cout << "Enter person ID number: ";
-//     int id;
-//     cin >> id;
-//     person->set_id(id);
-//     cin.ignore(256, '\n');
+// This function fills in a Person message based on user input.
+void PromptForAddress(tutorial::Person* person) {
+    cout << "Enter person ID number: ";
+    int id;
+    cin >> id;
+    person->set_id(id);
+    cin.ignore(256, '\n');
 
-//     cout << "Enter name: ";
-//     getline(cin, *person->mutable_name());
+    cout << "Enter name: ";
+    getline(cin, *person->mutable_name());
 
-//     cout << "Enter email address (blank for none): ";
-//     string email;
-//     getline(cin, email);
-//     if (!email.empty()) {
-//         person->set_email(email);
-//     }
+    cout << "Enter email address (blank for none): ";
+    string email;
+    getline(cin, email);
+    if (!email.empty()) {
+        person->set_email(email);
+    }
 
-//     while (true) {
-//         cout << "Enter a phone number (or leave blank to finish): ";
-//         string number;
-//         getline(cin, number);
-//         if (number.empty()) {
-//         break;
-//         }
+    while (true) {
+        cout << "Enter a phone number (or leave blank to finish): ";
+        string number;
+        getline(cin, number);
+        if (number.empty()) {
+        break;
+        }
 
-//         tutorial::Person::PhoneNumber* phone_number = person->add_phones();
-//         phone_number->set_number(number);
+        tutorial::Person::PhoneNumber* phone_number = person->add_phones();
+        phone_number->set_number(number);
 
-//         cout << "Is this a mobile, home, or work phone? ";
-//         string type;
-//         getline(cin, type);
-//         if (type == "mobile") {
-//         phone_number->set_type(tutorial::Person::PHONE_TYPE_MOBILE);
-//         } else if (type == "home") {
-//         phone_number->set_type(tutorial::Person::PHONE_TYPE_HOME);
-//         } else if (type == "work") {
-//         phone_number->set_type(tutorial::Person::PHONE_TYPE_WORK);
-//         } else {
-//         cout << "Unknown phone type.  Using default." << endl;
-//         }
-//     }
-// }
+        cout << "Is this a mobile, home, or work phone? ";
+        string type;
+        getline(cin, type);
+        if (type == "mobile") {
+        phone_number->set_type(tutorial::Person::PHONE_TYPE_MOBILE);
+        } else if (type == "home") {
+        phone_number->set_type(tutorial::Person::PHONE_TYPE_HOME);
+        } else if (type == "work") {
+        phone_number->set_type(tutorial::Person::PHONE_TYPE_WORK);
+        } else {
+        cout << "Unknown phone type.  Using default." << endl;
+        }
+    }
+}
 
 // Main function:  Reads the entire address book from a file,
 //   adds one person based on user input, then writes it back out to the same
 //   file.
 int main(int argc, char* argv[]) {
-    cout << "Program init" << endl;
     // Verify that the version of the library that we linked against is
     // compatible with the version of the headers we compiled against.
-    // GOOGLE_PROTOBUF_VERIFY_VERSION;
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
 
-    // if (argc != 2) {
-    //     cout << "Usage:  " << argv[0] << " ADDRESS_BOOK_FILE" << endl;
-    //     return -1;
-    // }
+    if (argc != 2) {
+        cout << "Usage:  " << argv[0] << " ADDRESS_BOOK_FILE" << endl;
+        return -1;
+    }
 
-    // tutorial::AddressBook address_book;
+    tutorial::AddressBook address_book;
 
-    // {
-    //     // Read the existing address book.
-    //     fstream input(argv[1], ios::in | ios::binary);
-    //     if (!input) {
-    //     cout << argv[1] << ": File not found.  Creating a new file." << endl;
-    //     } else if (!address_book.ParseFromIstream(&input)) {
-    //     cout << "Failed to parse address book." << endl;
-    //     return -1;
-    //     }
-    // }
+    {
+        // Read the existing address book.
+        fstream input(argv[1], ios::in | ios::binary);
+        if (!input) {
+        cout << argv[1] << ": File not found.  Creating a new file." << endl;
+        } else if (!address_book.ParseFromIstream(&input)) {
+        cout << "Failed to parse address book." << endl;
+        return -1;
+        }
+    }
 
-    // // Add an address.
-    // PromptForAddress(address_book.add_people());
+    // Add an address.
+    PromptForAddress(address_book.add_people());
 
-    // {
-    //     // Write the new address book back to disk.
-    //     fstream output(argv[1], ios::out | ios::trunc | ios::binary);
-    //     if (!address_book.SerializeToOstream(&output)) {
-    //     cout << "Failed to write address book." << endl;
-    //     return -1;
-    //     }
-    // }
+    {
+        // Write the new address book back to disk.
+        fstream output(argv[1], ios::out | ios::trunc | ios::binary);
+        if (!address_book.SerializeToOstream(&output)) {
+        cout << "Failed to write address book." << endl;
+        return -1;
+        }
+    }
 
-    // // Optional:  Delete all global objects allocated by libprotobuf.
-    // google::protobuf::ShutdownProtobufLibrary();
+    // Optional:  Delete all global objects allocated by libprotobuf.
+    google::protobuf::ShutdownProtobufLibrary();
 
     return 0;
 }
