097c936977c4a2d6b0be55e70056e7e3b7f5184d
Quark3e <b.erkhme@gmail.com>
2025-04-25 01:08:40 +0200

    need to create dataPlotter to visualise and potentially clean the data to remove empty entires.
    Might need to consider changing the column/row structure so motorID is a column and 1-180 angles are the columns to reduce empty columns

---
+++ projects/proj_Hexclaw_cpp/Hexclaw_Main/data/data_servoMotorDrift/dataPlotter/include/includes.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef HPP__HEXCLAW_MAIN__DATA_PLOTTER
+#define HPP__HEXCLAW_MAIN__DATA_PLOTTER
+
+#include <iostream>
+
+#include <fstream>
+
+#include <vector>
+#include <string>
+
+
+#include <imguiwin.hpp>
+
+
+#endif //HPP__HEXCLAW_MAIN__DATA_PLOTTER
