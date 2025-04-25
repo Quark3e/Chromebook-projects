cfed334d283aec992fc81564ef25fe5d0d992da6
Quark3e <b.erkhme@gmail.com>
2025-04-25 23:19:20 +0200

    moved dataPlotter to a new location to allow CMake to actually build.

---
+++ projects/proj_Hexclaw_cpp/dataPlotter/include/includes.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef HPP__HEXCLAW_MAIN__DATA_PLOTTER__INCLUDES
+#define HPP__HEXCLAW_MAIN__DATA_PLOTTER__INCLUDES
+
+#include <iostream>
+
+#include <fstream>
+
+#include <vector>
+#include <string>
+
+
+#include <pos2d.hpp>
+
+#include "imguiwin.hpp"
+
+#include <keyBind_handler.hpp>
+#include <nodeChart/extra_imgui.hpp>
+
+
+#endif //HPP__HEXCLAW_MAIN__DATA_PLOTTER__INCLUDES
---
+++ projects/proj_Hexclaw_cpp/dataPlotter/src/global_variables.cpp
@@ -0,0 +1,24 @@
+
+#include "global_variables.hpp"
+
+
+GUINC::imguiwin guiwin(false);
+
+void _keyBind__Master_Close() {
+    guiwin.exit();
+}
+DIY_KBH::keyBind_handler keyBinds({
+    {"Master Close",    {ImGuiKey_ReservedForModCtrl, ImGuiKey_LeftCtrl, ImGuiKey_W}, _keyBind__Master_Close},
+    {"MouseLeft",       {ImGuiKey_MouseLeft}},
+    {"MouseRight",      {ImGuiKey_MouseRight}}
+});
+pressed_key__struct guiKeys;
+
+int keyAction_mouseLeft = -1;
+
+pos2d<int> DIM_PROGRAM__WIN(1000, 1000);
+
+
+pos2d<int> DIM_PROGRAM__MOTOR_SELECT_PANEL(200, 1000);
+
+
